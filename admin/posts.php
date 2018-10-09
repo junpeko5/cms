e0db37e04b3f82d8a86829eba8bf857739756142
Junpei Konishi <junpeko5@gmail.com>
2018-10-10 05:58:57 +0900

    postsページの表示

--- admin/posts.php
+++ admin/posts.php
@@ -26,15 +26,36 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td>test</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <?php
+                                $query = "SELECT * FROM posts";
+                                $select_posts = mysqli_query($connection, $query);
+
+                                while ($row = mysqli_fetch_assoc($select_posts)) {
+                                    $post_id = $row['post_id'];
+                                    $post_author = $row['post_author'];
+                                    $post_title = $row['post_title'];
+                                    $post_category_id = $row['post_category_id'];
+                                    $post_status = $row['post_status'];
+                                    $post_image = $row['post_image'];
+                                    $post_tags = $row['post_tags'];
+                                    $post_comment_count = $row['post_comment_count'];
+                                    $post_date = $row['post_date'];
+                                    echo '<tr>';
+                                    echo "<td>$post_id</td>";
+                                    echo "<td>$post_author</td>";
+                                    echo "<td>$post_title</td>";
+                                    echo "<td>$post_category_id</td>";
+                                    echo "<td>$post_status</td>";
+                                    echo "<td>
+                                            <img class='img-responsive' 
+                                                 src='../images/$post_image'>
+                                         </td>";
+                                    echo "<td>$post_tags</td>";
+                                    echo "<td>$post_comment_count</td>";
+                                    echo "<td>post_date</td>";
+                                    echo '</tr>';
+                                }
+                                ?>
                             </tr>
                         </tbody>
                     </table>
@@ -44,4 +65,3 @@
     </div>
 </div>
 <?php include("includes/admin_footer.php"); ?>
-
