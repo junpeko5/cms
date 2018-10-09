be002f201f0fbe3622880d6ee3bdba64792b2d21
Junpei Konishi <junpeko5@gmail.com>
2018-10-10 00:04:38 +0900

    postsページの作成

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -42,12 +42,15 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">Dropdown Item</a>
+                        <a href="/cms/admin/posts.php">View All Posts</a>
                     </li>
                     <li>
-                        <a href="#"></a>
+                        <a href="#">Add Post</a>
                     </li>
                 </ul>
+            </li>
+            <li>
+                <a href="/cms/admin/categories.php"><i class="fa fa-fw fa-dashboard"></i> Categories</a>
             </li>
             <li class="active">
                 <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
---
+++ admin/posts.php
@@ -0,0 +1,47 @@
+<?php include("includes/admin_header.php"); ?>
+<div id="wrapper">
+    <?php include("includes/navigation.php"); ?>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Posts
+                        <small>Author</small>
+                    </h1>
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>test</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("includes/admin_footer.php"); ?>
+
