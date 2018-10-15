9965fd329fb9d227cbf71a6dbbdca4bf403c8603
Junpei Konishi <junpeko5@gmail.com>
2018-10-15 13:00:52 +0900

    CMSでusersページの作成

---
+++ admin/includes/add_user.php
@@ -0,0 +1,88 @@
+<?php
+if (isset($_POST['create_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['post_image']['name'];
+    $post_image_tmp = $_FILES['post_image']['tmp_name'];
+    $post_content = mysqli_real_escape_string($connection, $_POST['post_content']);
+    $post_tags = $_POST['post_tags'];
+    $post_comment_count = 0;
+    $post_date = date('d-m-y');
+    move_uploaded_file($post_image_tmp, "../images/$post_image");
+
+    $query = "
+        INSERT INTO
+            posts
+        (
+            post_category_id, 
+            post_title, 
+            post_author, 
+            post_date, 
+            post_image, 
+            post_content, 
+            post_tags, 
+            post_comment_count,
+            post_status
+        )
+        VALUES
+        (
+            {$post_category_id},
+            '{$post_title}',
+            '{$post_author}',
+            now(),
+            '{$post_image}',
+            '{$post_content}',
+            '{$post_tags}',
+            {$post_comment_count},
+            '{$post_status}'
+        )   
+    ";
+    confirmQuery($query);
+}
+?>
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="post_title">Post Title</label>
+        <input id="post_title" type="text" name="post_title" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="post_author">Post Author</label>
+        <input id="post_author" type="text" name="post_author" class="form-control">
+    </div>
+    <label for="post_category_id">Post Category</label>
+    <select name="post_category_id" id="post_category_id">
+        <?php
+        $query = "SELECT * FROM categories";
+        $result = confirmQuery($query);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
+
+            echo "<option value='{$cat_id}'>{$cat_title}</option>";
+        }
+        ?>
+    </select>
+    <div class="form-group">
+        <label for="title">Post Status</label>
+        <input id="title" type="text" name="post_status" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input id="post_image" type="file" name="post_image" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input id="post_tags" type="text" name="post_tags" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea id="post_content" name="post_content" class="form-control" rows="5"></textarea>
+    </div>
+    <div class="form-group">
+        <input type="submit" name="create_post" value="Create Posts" class="btn btn-primary">
+    </div>
+</form>
--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -59,10 +59,10 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#users_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="users_dropdown" class="collapse">
                     <li>
-                        <a href="#">Dropdown Item</a>
+                        <a href="/cms/admin/users.php">View All users</a>
                     </li>
                     <li>
-                        <a href="#">Dropdown</a>
+                        <a href="/cms/admin/users.php?source=add_user">Add User</a>
                     </li>
                 </ul>
             </li>
