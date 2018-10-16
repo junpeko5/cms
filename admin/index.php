452ccf5ad30a61aaafd1ac9afbbd3aa833635621
Junpei Konishi <junpeko5@gmail.com>
2018-10-16 22:32:32 +0900

    widgetのcountを表示

--- admin/index.php
+++ admin/index.php
@@ -21,7 +21,14 @@
                                         <i class="fa fa-file-text fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>12</div>
+                                        <?php
+                                        $query = "
+                                            SELECT * FROM posts
+                                        ";
+                                        $select_all_post = confirmQuery($query);
+                                        $post_count = mysqli_num_rows($select_all_post);
+                                        ?>
+                                        <div class='huge'><?php echo $post_count; ?></div>
                                         <div>Posts</div>
                                     </div>
                                 </div>
@@ -43,7 +50,14 @@
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>23</div>
+                                        <?php
+                                        $query = "
+                                            SELECT * FROM comments
+                                        ";
+                                        $select_all_comment = confirmQuery($query);
+                                        $comment_count = mysqli_num_rows($select_all_comment);
+                                        ?>
+                                        <div class='huge'><?php echo $comment_count; ?></div>
                                         <div>Comments</div>
                                     </div>
                                 </div>
@@ -65,7 +79,14 @@
                                         <i class="fa fa-user fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>23</div>
+                                        <?php
+                                        $query = "
+                                            SELECT * FROM users
+                                        ";
+                                        $select_all_users = confirmQuery($query);
+                                        $user_count = mysqli_num_rows($select_all_users);
+                                        ?>
+                                        <div class='huge'><?php echo $user_count; ?></div>
                                         <div> Users</div>
                                     </div>
                                 </div>
@@ -87,7 +108,14 @@
                                         <i class="fa fa-list fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class='huge'>13</div>
+                                        <?php
+                                        $query = "
+                                            SELECT * FROM categories
+                                        ";
+                                        $select_all_categories = confirmQuery($query);
+                                        $category_count = mysqli_num_rows($select_all_categories);
+                                        ?>
+                                        <div class='huge'><?php echo $category_count; ?></div>
                                         <div>Categories</div>
                                     </div>
                                 </div>
