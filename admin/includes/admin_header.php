3488a272f4d932bb2df3c888f65e11f744e646a7
Junpei Konishi <junpeko5@gmail.com>
2018-10-24 22:00:28 +0900

    load時のアニメーションを追加

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -22,6 +22,8 @@ if (!isset($_SESSION['user_role'])) {
     <link href="css/sb-admin.css" rel="stylesheet">
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/cms/admin/css/styles.css" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
