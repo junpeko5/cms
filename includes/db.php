ac9256d44df61b5bf871a9d6444901e56fcd9f2e
junpeko5 <junpeko5@gmail.com>
2018-10-29 01:00:58 +0900

    DB設定の記述をdotenvに変更

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
 
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = 'root';
-$db['db_name'] = 'cms';
+$db['db_host'] = getenv('DB_HOST');
+$db['db_user'] = getenv('DB_USER');
+$db['db_pass'] = getenv('DB_PASS');
+$db['db_name'] = getenv('DB_NAME');
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
 }
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$connection = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
 
 //if ($connection) {
 //    echo "We are connected";
